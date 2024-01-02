9f4c54aa4d57dd1de15a3ec13492e8aee63a4189
Peng Du <rick.peng.du@gmail.com>
2024-01-02 11:27:49 -0800

    add delete binary tree

--- src/ACT_1_cpp_basics/1.4_bubble_sort/bubble_sort.cpp
+++ src/ACT_1_cpp_basics/1.4_bubble_sort/bubble_sort.cpp
@@ -1,23 +1,22 @@
 #include "bubble_sort.h"
 
 void bubble_sort(int *data, int length) {
-    if (data == nullptr || length < 2) {
+    if (length == 0 || data == nullptr) {
         return;
     }
 
-    // how many swaps in total?
-    for (int i = 0; i < length; i++) {
-        for (int j = 0; j < length - i - 1; j++) {
+    for (int i = 0; i < length; ++i) {
+        for (int j = 0; j < length - 1; ++j) {
             if (data[j] > data[j + 1]) {
-                swap(data, j, j + 1);
+                swap(data + j, data + j + 1);
             }
         }
     }
+
 }
 
-void swap(int *data, int i, int j) {
-    // can you do the following code without using any additional variable?
-    int tmp = data[i];
-    data[i] = data[j];
-    data[j] = tmp;
+void swap(int *v1, int *v2) {
+    int tmp = *v1;
+    *v1 = *v2;
+    *v2 = tmp;
 }
--- src/ACT_1_cpp_basics/1.4_bubble_sort/bubble_sort.h
+++ src/ACT_1_cpp_basics/1.4_bubble_sort/bubble_sort.h
@@ -1,3 +1,5 @@
 void bubble_sort(int *data, int length);
 
-void swap(int *data, int i, int j);
+void swap(int *v1, int *v2);
+
+//void swap(int *data, int i, int j);
